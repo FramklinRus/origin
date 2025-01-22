bf42c453f597641578536ee3182fa1acccdf9667
Ruslan B <super.konor2015@mail.ru>
2025-01-22 21:25:46 +0500

    Change zadanie 1

--- zadanie1/zadanie_1/zadanie_1/zadanie_1.cpp
+++ zadanie1/zadanie_1/zadanie_1/zadanie_1.cpp
@@ -1,70 +1,63 @@
-﻿// zadanie_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-enum class Month
-{
-    январь=1,
-    февраль,
-    март,
-    апрель,
-    май,
-    июнь,
-    июль,
-    август,
-    сентябрь,
-    октябрь,
-    ноябрь,
-    декабрь,
-    ошибка
-
+enum class Month {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
 };
-Month get_month(int M)
-{
-    
-    if (M >= 1 && M <= 12)
-    {
-        return static_cast<Month>(M); 
+
+Month get_month(int M) {
+    if (M >= 1 && M <= 12) {
+        return static_cast<Month>(M);
     }
-    return Month::ошибка;
+    return Month::January; // Вернем любое значение, т.к. обработка ошибки в main
 }
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Russian");
-        
-while (x != 0)
-    {
-        cout << "Введите номер месяца:";
+
+    int x = -1; // Инициализация переменной
+
+    while (x != 0) {
+        cout << "Введите номер месяца: ";
         cin >> x;
 
-
-        switch (get_month(x))
-        {
-        case Month::январь: cout << "Январь" << endl;break;
-        case Month::февраль: cout << "февраль" << endl;break;
-        case Month::март: cout << "март" << endl;break;
-        case Month::апрель: cout << "апрель" << endl;break;
-        case Month::май: cout << "май" << endl;break;
-        case Month::июнь: cout << "июнь" << endl;break;
-        case Month::июль: cout << "июль" << endl;break;
-        case Month::август: cout << "август" << endl;break;
-        case Month::сентябрь: cout << "сентябрь" << endl;break;
-        case Month::октябрь: cout << "октябрь" << endl;break;
-        case Month::ноябрь: cout << "ноябрь" << endl;break;
-        case Month::декабрь: cout << "декабрь" << endl;break;
-        case Month::ошибка: cout << "ошибка" << endl;break;
-
-
+        if (x == 0) {
+            cout << "До свидания" << endl;
+            break;
         }
-    
 
-}
-    
-    
-        cout << "До свидания" << endl;
-    
-    
+        if (x < 1 || x > 12) {
+            cout << "Ошибка: такого месяца не существует!" << endl;
+            continue;
+        }
+
+        switch (get_month(x)) {
+        case Month::January: cout << "Январь" << endl; break;
+        case Month::February: cout << "Февраль" << endl; break;
+        case Month::March: cout << "Март" << endl; break;
+        case Month::April: cout << "Апрель" << endl; break;
+        case Month::May: cout << "Май" << endl; break;
+        case Month::June: cout << "Июнь" << endl; break;
+        case Month::July: cout << "Июль" << endl; break;
+        case Month::August: cout << "Август" << endl; break;
+        case Month::September: cout << "Сентябрь" << endl; break;
+        case Month::October: cout << "Октябрь" << endl; break;
+        case Month::November: cout << "Ноябрь" << endl; break;
+        case Month::December: cout << "Декабрь" << endl; break;
+        default: cout << "Ошибка!" << endl; break;
+        }
+    }
+
+    return 0;
 }
